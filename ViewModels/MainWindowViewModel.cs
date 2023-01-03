c3ebe037387fdbbb3d5d63e5cff1143c260c9233
FakeRayBanz <matthewparker@hotmail.com.au>
2023-01-03 23:37:44 +1000

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace Cadence.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+        public ICommand StartTracking { get; }
+
+        public MainWindowViewModel()
+        {
+            StartTracking = ReactiveCommand.Create(() =>
+            {
+                Console.WriteLine("Test");
+            });
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Cadence.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
